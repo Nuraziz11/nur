8a91c48c943d2a47565bfddc15b829a712a874af
Nuraziz11 <132667941+Nuraziz11@users.noreply.github.com>
2023-05-05 17:10:46 +0500

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Blog
+# Create your views here.
+def home(request):
+    blog = Blog.objects.all()
+    return render(request,'index.html',{"blog":blog})
