3260b836a61ec8171160652b8b49c5f81db0154c
Nuraziz11 <132667941+Nuraziz11@users.noreply.github.com>
2023-05-08 13:44:56 +0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=500)
     body = models.TextField()
-     
+    def __str__(self):
+        return self.title
